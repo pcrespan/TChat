32b597431e545ebbb61f3f034c7153a942ed8608
Pedro Crespan <pedrocrespan@hotmail.com>
2022-11-03 18:14:55 -0300

    Server is now a class. Disconnect now works properly

--- Server.py
+++ Server.py
@@ -4,62 +4,61 @@ import threading
 from Chat import getSocket
 
 
-global clients
-clients = {}
-global connectedClients
-global threads
-threads = {}
-connectedClients = clients
+class Server:
+    def __init__(self):
+        self.clients = []
+        self.connectedClients = []
+        self.sock = getSocket()
 
-def bindSocket(sock, serverIP, port):
-    sock.bind((serverIP, port))
-
-
-def socketListen(sock, n):
-    sock.listen(n)
+    def bindSocket(self, serverIP, port):
+        self.sock.bind((serverIP, port))
 
 
-def acceptConnection(sock):
-    while True:
-        con, senderIP = sock.accept()
-        print(f"{senderIP} connected to the server")
-        clients[con] = senderIP
-        threads[con] = threading.Thread(target=clientCommunication, args=(con, ))
-        threads[con].start()
-    
-
-def clientCommunication(con):
-    while True:
-        msg = con.recv(1024)
-        if len(msg) == 0:
-            print("Conexao fechou")
-            threads[con].join()
-            break
-        communicateServer(msg)
+    def socketListen(self, n):
+        self.sock.listen(n)
 
 
-def communicateServer(msg):
-    # Add try except to remove connections
-    global clients
-    global connectedClients
-    connectedClients = clients
-    for client in clients:
-        try:
-            client.send(msg)
-        except:
-            print(f"{clients[client]} has disconnected")
-            connectedClients.pop(client)
-    clients = connectedClients
+    def acceptConnection(self):
+        while True:
+            con, senderIP = self.sock.accept()
+            print(f"{senderIP} connected to the server")
+            self.clients.append(con)
+            print(self.clients)
+            threading.Thread(target=self.clientCommunication, args=(con, )).start()
+            
+
+    def clientCommunication(self, con):
+        while True:
+            msg = con.recv(1024)
+            if len(msg) == 0:
+                print("Conexao fechou")
+                break
+            self.communicateServer(msg)
+
+
+    def communicateServer(self, msg):
+        # Add try except to remove connections
+        self.connectedClients = self.clients
+        i = 0
+        for client in self.clients:
+            try:
+                client.send(msg)
+                print("enviando")
+                i += 1
+            except:
+                print("A client has disconnected")
+                self.connectedClients.pop(i)
+        self.clients = self.connectedClients
 
 
 def main():
     serverIP = "127.0.0.1"
     port = int(input("Port to listen for connections: "))
-
+    server = Server()
     sock = getSocket()
-    bindSocket(sock, serverIP, port)
-    socketListen(sock, 1)
-    acceptConnection(sock)
+    server.bindSocket(serverIP, port)
+    server.socketListen(1)
+    server.acceptConnection()
 
     
 if __name__ == "__main__":
